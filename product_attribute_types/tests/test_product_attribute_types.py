a6e3b8a903908556a4a26428aed0d24eecdad91f
onurugur <onurugur@gitlab.com>
2019-03-28 11:12:04 +0300

    odoomrp-wip

--- product_attribute_types/tests/test_product_attribute_types.py
+++ product_attribute_types/tests/test_product_attribute_types.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # © 2016 Mikel Arregi Etxaniz - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-import openerp.tests.common as common
+import odoo.tests.common as common
 
 
 class TestProductAttributeTypes(common.TransactionCase):
---
+++ sale_order_recalculate_prices/models/sale_order.py
@@ -0,0 +1,37 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#                                                                            #
+#  OpenERP, Open Source Management Solution.                                 #
+#                                                                            #
+#  @author Carlos Sánchez Cifuentes <csanchez@grupovermon.com>               #
+#                                                                            #
+#  This program is free software: you can redistribute it and/or modify      #
+#  it under the terms of the GNU Affero General Public License as            #
+#  published by the Free Software Foundation, either version 3 of the        #
+#  License, or (at your option) any later version.                           #
+#                                                                            #
+#  This program is distributed in the hope that it will be useful,           #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of            #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the              #
+#  GNU Affero General Public License for more details.                       #
+#                                                                            #
+#  You should have received a copy of the GNU Affero General Public License  #
+#  along with this program. If not, see <http://www.gnu.org/licenses/>.      #
+#                                                                            #
+##############################################################################
+
+from odoo import models, api
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    @api.multi
+    def recalculate_prices(self):
+        for record in self:
+            if record.order_line:
+                for line in record.order_line:
+                    res = line.product_id_change(
+                        )
+                   
+        return True
